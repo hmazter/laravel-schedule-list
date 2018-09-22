d166ce5821d1344e308241304a63a91a80668bad
Kristoffer Högberg <krihog@gmail.com>
2018-09-22 20:15:40 +0200

    Handle scheduled Closures and Jobs

--- tests/Integration/MockConsoleKernel.php
+++ tests/Integration/MockConsoleKernel.php
@@ -8,8 +8,14 @@ class MockConsoleKernel extends \Orchestra\Testbench\Console\Kernel
 
     protected function schedule(\Illuminate\Console\Scheduling\Schedule $schedule)
     {
+        $closure = function () {
+            echo 'callback or invokable class';
+        };
+
         $schedule->command('test:command:name')->dailyAt('10:00')->description('Description of event');
         $schedule->command('test:command:two')->dailyAt('10:00')->timezone('UTC');
         $schedule->exec('ls -lah')->mondays()->at('3:00');
+        $schedule->call($closure)->dailyAt('13:00')->description('A description for a scheduled callback');
+        $schedule->job(new \TestJob())->dailyAt('14:00');
     }
 }
