00cef51c889f47b216c6b5b0c9e6431f5efb789c
Kristoffer Högberg <krihog@gmail.com>
2019-09-08 19:30:14 +0200

    PHPUnit 7 or 8 depending on Orchestra version

--- tests/Integration/ListSchedulerTest.php
+++ tests/Integration/ListSchedulerTest.php
@@ -20,24 +20,24 @@ class ListSchedulerTest extends TestCase
         $consoleOutput = explode("\n", trim(\Illuminate\Support\Facades\Artisan::output()));
         $cron = \Cron\CronExpression::factory('0 10 * * *');
 
-        self::assertContains('test:command:name', $consoleOutput[3]);
-        self::assertContains('Description of event', $consoleOutput[3]);
-        self::assertContains('0 10 * * *', $consoleOutput[3]);
-        self::assertContains($cron->getNextRunDate()->format('Y-m-d H:i:s'), $consoleOutput[3]);
+        self::assertStringContainsString('test:command:name', $consoleOutput[3]);
+        self::assertStringContainsString('Description of event', $consoleOutput[3]);
+        self::assertStringContainsString('0 10 * * *', $consoleOutput[3]);
+        self::assertStringContainsString($cron->getNextRunDate()->format('Y-m-d H:i:s'), $consoleOutput[3]);
 
         // get description from the command class
-        self::assertContains('test:command:two', $consoleOutput[4]);
-        self::assertContains('Description of test command', $consoleOutput[4]);
+        self::assertStringContainsString('test:command:two', $consoleOutput[4]);
+        self::assertStringContainsString('Description of test command', $consoleOutput[4]);
 
-        self::assertContains('ls -lah', $consoleOutput[5]);
+        self::assertStringContainsString('ls -lah', $consoleOutput[5]);
 
-        self::assertContains('0 13 * * *', $consoleOutput[6]);
-        self::assertContains('Closure', $consoleOutput[6]);
-        self::assertContains('A description for a scheduled callback', $consoleOutput[6]);
+        self::assertStringContainsString('0 13 * * *', $consoleOutput[6]);
+        self::assertStringContainsString('Closure', $consoleOutput[6]);
+        self::assertStringContainsString('A description for a scheduled callback', $consoleOutput[6]);
 
-        self::assertContains('0 14 * * *', $consoleOutput[7]);
-        self::assertContains('Closure', $consoleOutput[7]);
-        self::assertContains('TestJob', $consoleOutput[7]);
+        self::assertStringContainsString('0 14 * * *', $consoleOutput[7]);
+        self::assertStringContainsString('Closure', $consoleOutput[7]);
+        self::assertStringContainsString('TestJob', $consoleOutput[7]);
     }
 
     public function testListSchedulerCommand_withTasksAndCronStyle()
@@ -45,17 +45,17 @@ class ListSchedulerTest extends TestCase
         \Illuminate\Support\Facades\Artisan::call('schedule:list', ['--cron' => true]);
         $consoleOutput = explode("\n", trim(\Illuminate\Support\Facades\Artisan::output()));
 
-        self::assertContains('test:command:name', $consoleOutput[0]);
-        self::assertContains('artisan', $consoleOutput[0]);
-        self::assertContains('0 10 * * *', $consoleOutput[0]);
-        self::assertContains((DIRECTORY_SEPARATOR === '\\') ? 'NUL' : '/dev/null', $consoleOutput[0]);
+        self::assertStringContainsString('test:command:name', $consoleOutput[0]);
+        self::assertStringContainsString('artisan', $consoleOutput[0]);
+        self::assertStringContainsString('0 10 * * *', $consoleOutput[0]);
+        self::assertStringContainsString((DIRECTORY_SEPARATOR === '\\') ? 'NUL' : '/dev/null', $consoleOutput[0]);
 
-        self::assertContains('test:command:two', $consoleOutput[1]);
+        self::assertStringContainsString('test:command:two', $consoleOutput[1]);
 
-        self::assertContains('ls -lah', $consoleOutput[2]);
+        self::assertStringContainsString('ls -lah', $consoleOutput[2]);
 
-        self::assertContains('Closure', $consoleOutput[3]);
+        self::assertStringContainsString('Closure', $consoleOutput[3]);
 
-        self::assertContains('Closure', $consoleOutput[4]);
+        self::assertStringContainsString('Closure', $consoleOutput[4]);
     }
 }
